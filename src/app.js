596daa88de465d9ad41c00ec8eca3247b1ba7984
pablopunk <pablovarela182@gmail.com>
2019-06-29 20:12:25 +0200

    Hide dock icon again

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const { newServerEvent } = require('./server')
 const iconPath = path.join(__dirname, '..', 'assets/iconTemplate.png')
 
 app.on('ready', _ => {
-  // app.dock.hide()
+  app.dock.hide()
   const tray = createTray(iconPath)
   tray.setToolTip(`Open ${app.getName()}`)
   tray.on('drop-files', (_, files) => {
